b0c9bd30dc0bc7a5b84cb40e63f70f65a1836b05
Alaina <alaismit1@gmail.com>
2021-11-30 15:39:40 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const ctrl = require('./controller.js')
+
+// const {
+//     createHouse,
+//     updateHouse,
+//     deleteHouse
+// } = require(`.controller`)
+
+app.get(`/api/houses`, ctrl.getAllHouses)
+app.post(`/api/houses/`, ctrl.createHouse)
+app.put(`/api/houses/:id`, ctrl.updateHouse)
+app.delete(`/api/houses/:id`, ctrl.deleteHouse)
+
+app.listen(5000, () => console.log('running on 5000'))
